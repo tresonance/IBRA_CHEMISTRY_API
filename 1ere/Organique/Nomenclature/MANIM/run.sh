a2f62c71500d17a13ae945d1fa9ca52ca5f6eee7
tresonance <tresonance2@gmail.com>
2022-08-06 06:20:50 +0200

    update with new format

--- 1ere/Organique/Nomenclature/MANIM/run.sh
+++ 1ere/Organique/Nomenclature/MANIM/run.sh
@@ -34,12 +34,14 @@ class_level="1ere"
 # set image name (see Dockerfile)
 my_manim_image="chemistry_image"
 # set container name
-my_manim_container_name="my-chemistry-container"
+my_manim_container_name="my-chemistry-tle-container"
 
-echo -e "${YELLOW} Info: To removed container run: ./run.sh down ${RESET}\n"
-echo -e "${YELLOW} Info: To save videos to volume:  run: ./run.sh save ${RESET}\n"
-echo -e "${YELLOW} Info: To build MANIM videos ./run.sh  my-python_script.py ${RESET}\n"
-echo -e "${YELLOW} Info: To build corresponding docker image:  run: ./run.sh build ${RESET}\n"
+# Suject name in DISKE (where we must save this videos )
+#Exemple "Physic" in /Volumes/DiskE/VIDEOS-1ere-Physic-mp4
+diske_topic="Chemistry" 
+
+#  variable GIT_MY_CHANEL_NAME, #Exemple: GIT_MY_CHANNELS_MATHS
+GIT_MY_CHANEL_NAME=$(pwd | cut -d "/" -f 4) 
 
 # save python script file
 PYTHON_SCRIPT_FILE=""
@@ -47,7 +49,16 @@ PYTHON_SCRIPT_FILE=""
 # recall Usage
 if [ "$#" -eq 0 ]; 
 then 
-    echo -e "\n${ONCOLOR_FAILED} [To run python script] Usage ./runs.sh my_python_file.py ${RESET}\n\n${ONCOLOR_FAILED} [To exit from container ] Usage  ./runs.sh down ${RESET} \n\n${ONCOLOR_FAILED}[To save videos to Volume  ] Usage  ./runs.sh save ${RESET} \n"
+    #echo -e "\n${ONCOLOR_FAILED} [To run python script] Usage ./runs.sh my_python_file.py ${RESET}\n\n${ONCOLOR_FAILED} [To exit from container ] Usage  ./runs.sh down ${RESET} \n\n${ONCOLOR_FAILED}[To save videos to Volume  ] Usage  ./runs.sh save ${RESET} \n"
+    echo -e "${RED} ........................ U S A G E .................... ${RESET}\n"
+    echo -e "${ONPURPLE} PLEASE, MOUNT YOUR DiskE BEFORE ${RESET}\n"
+    echo -e "${ONCYAN} Info: To removed container run: ./run.sh down \t\t\t ${RESET}\n"
+    echo -e "${ONPURPLE} Info: To save videos to volume:  run: ./run.sh save \t\t ${RESET}\n"
+    echo -e "${ONCYAN} Info: To build MANIM videos ./run.sh  my-python_script.py \t ${RESET}\n"
+    echo -e "${ONPURPLE} Info: To build corresponding docker image:  run: ./run.sh build ${RESET}\n"
+    echo -e "${ONCYAN} Info: To create ONLY_BOARD symlink:  run: ./run.sh symlink \t ${RESET}\n"
+    echo -e "${RED} .......................END  U S A G E .................... ${RESET}\n"
+
     return 
 fi 
 
@@ -60,17 +71,18 @@ then
 elif [ "$1" == "save" ];
 then 
         # Mount Volume 
-        MOUNT_FILES="$(mount -t /dev/disk2s1 && ls  "/Volumes/Disk E"  )"
+        MOUNT_FILES="$(mount -t /dev/disk2s1 && ls  "/Volumes/DiskE"  )"
         if [ "$?" != 0 ];
         then 
-            echo -e "${ONCOLOR_FAILED} [ERROR]: Disk E not found ${RESET} \n"
+            echo -e "${ONCOLOR_FAILED} [ERROR]: DiskE not found ${RESET} \n"
             return 
         fi 
        
-        #VOLUME_VIDEOS_PATH="/Volumes/Disk E/VIDEOS-1ere-Maths-mp4/mp4_trigo"
-        VOLUME_VIDEOS_PATH="/Volumes/Disk E/VIDEOS-${class_level}-Chemistry-mp4/"
+        #VOLUME_VIDEOS_PATH="/Volumes/DiskE/VIDEOS-1ere-Maths-mp4/mp4_trigo"
+        VOLUME_VIDEOS_PATH="/Volumes/DiskE/VIDEOS-${class_level}-${diske_topic}-mp4/"
         #MANIM_VIDEOS_BASE_PATH="/Users/whodunit/MY_CHANNELS_MATHS/1ere/Geometry/VIDEO-1ere-PRO-SCALAIRE-1-2-3-4/MANIM/media/videos"
-        MANIM_VIDEOS_BASE_PATH="/Users/whodunit/MY_CHANNELS_CHEMISTRY/${class_level}/Organique/Nomenclature/MANIM/media/videos"
+        #MANIM_VIDEOS_BASE_PATH="/Users/whodunit/${GIT_MY_CHANEL_NAME}/${class_level}/MANIM/media/videos"
+        MANIM_VIDEOS_BASE_PATH="$(pwd)/media/videos"
 
         #MP4_FILES_BASE_NAME=$(find ${MANIM_VIDEOS_BASE_PATH} -type f -name "[a-zA-Z]*.mp4" | while read line; do basename $line ;done )
         MP4_FILES_ABSOLUTE_NAME=$(find ${MANIM_VIDEOS_BASE_PATH} -type f -name "[a-zA-Z]*.mp4" )
@@ -100,9 +112,21 @@ elif [ "$1" == "build" ]; #this is specific for chemistry because it is our own 
 then 
     echo -e "${ONBLUE} START BUILDING DOCKER CONTAINER IMAGE ${my_manim_image}  ${RESET} \n"
     $(docker build -t $my_manim_image .)
-    echo -e "...............Finish created New chemistry docker image ............\n"
+    echo -e "...............Finish created New Physic docker image ............\n"
     $(docker images | grep $my_manim_image)
     return
+elif [ "$1" == "symlink" ]
+then 
+    #$(rm -rf EXTERN_GEOMETRIE/ONLY_BOARD && rm -rf ../ONLY_BOARD)
+    #$(ln -s /Users/whodunit/GIT_ONLY_BOARD_GPU/ ONLY_BOARD > /dev/null 2>&1)
+    $(ln -s /Users/whodunit/GIT_ONLY_BOARD_GPU/ ONLY_BOARD )
+    $(rm -rf ../ONLY_BOARD && ../EXTERN_GEOMETRIE/ONLY_BOARD > /dev/null 2>&1 )
+    $(mv ONLY_BOARD ../ > /dev/null 2>&1 )
+    
+    echo -e "${ONBLUE} ONLY_BOARD@ link has been created ${RESET} \n"
+    result=$(ls -lrt ../| grep ONLY_BOARD)
+    echo -e "${CYAN} ${result} ${RESET}\n"
+    return 
 fi
 
 
@@ -145,7 +169,8 @@ if [ "$( docker inspect -f '{{.State.Status}}' $my_manim_container_name  2>/dev/
 then
     echo -e "${BLUE}Estimated time : 33m2.879s ${RESET}\n"
     #docker exec -it --user="$(id -u):$(id -g)" $my_manim_container_name  manim "/manim/$PYTHON_SCRIPT_FILE"
-    docker exec -it --user="$(id -u):$(id -g)" ${my_manim_container_name} manim -p -qm $PYTHON_SCRIPT_FILE 
+    #docker exec -it  ${my_manim_container_name} manim -p -qm --disable_caching $PYTHON_SCRIPT_FILE ChanimScene
+    docker exec -it  ${my_manim_container_name} manim -p -qm  $PYTHON_SCRIPT_FILE ChanimScene
 else 
      echo -e "${ONCOLOR_FAILED} FAILED TO RUN CONTAINER ${RESET}\n"
 fi
